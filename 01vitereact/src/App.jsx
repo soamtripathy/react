030392fbab65cdff3525b6307925a6b5a5398719
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-04-09 20:28:51 +0530

    jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,13 @@
 import Chai from "./chai"
 function App() {
 
+  const username = "Soam Tripathy"
   return (
     //fragment in react
     <> 
     <Chai/>
-    <h1>Chai aur code</h1>
+    <h1>Chai aur code {username}</h1>
+    {/* evaluted expression --> {username} */}
     <p>Test</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function MyApp() {
+  return (
+    <>
+      <h1>Custom App</h1>
+    </>
+  );
+}
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Google
+  </a>
 );
+const anotherUser = "CHai aur code"
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit google", 
+  anotherUser
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
