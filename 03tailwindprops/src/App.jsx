52f6832525d8fabcb8c3d01e60ea6a5b4552ba38
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-04-14 20:15:37 +0530

    tailwind css and props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./component/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let myObj = {
+    username : "Soumya",
+    age : 23
+  }
+  let newArray = [1, 2, 3]
   return (
     <>
-      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-10">
+        Tailwind test
+      </h1>
+      <Card username = "chai aur code" btnText = "Visit me" />
+      <Card username = "Soumya" btnText={"Click Me"}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
