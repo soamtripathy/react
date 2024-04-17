48e82460b025264e134a2908c9375e7721686277
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-04-17 14:50:20 +0530

    completed

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,16 +1,47 @@
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("blue");
+  const [color, setColor] = useState("black");
 
   return (
     <div
-      className="w-full h-screen duration-200"
+      className="w-full h-screen duration-200 bg-red-500"
       style={{ backgroundColor: color }}
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl text-red-500">
-          Test
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl ">
+          <button
+            onClick={()=>setColor("red")}
+            // onclick wants only function
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button onClick={()=>setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button onClick={()=>setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button onClick={()=>setColor("pink")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </button>
+          <button onClick={()=>setColor("violet")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "violet" }}
+          >
+            Violet
+          </button>
         </div>
       </div>
     </div>
