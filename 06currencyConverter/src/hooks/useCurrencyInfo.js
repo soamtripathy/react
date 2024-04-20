c16cc16bfccd13ed586ca80f2c7cbe4988ee4408
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-04-20 16:14:13 +0530

    UI & Functinality

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -3,11 +3,10 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency) {
     const [data, setData] = useState({});
     useEffect(() => {
-        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
             .then((res) => res.json())
-            .then((res) => setData(res.rates[currency]));
+            .then((res) => setData(res[currency]));
     }, [currency]);
-    console.log(data);
     return data;
 }
 
