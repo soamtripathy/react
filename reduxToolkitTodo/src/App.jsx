dc40ced6fb01a77e21bdf579c544b61a0acceed9
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-05-05 23:13:16 +0530

    redux toolkit complete

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <>
       <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todo />
     </>
   );
 }
 
 export default App;
-  
--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -18,16 +18,6 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
-    updateTodo: (state, action) => {
-      const { id, text } = action.payload;
-      const todo = state.todos.find((todo) => todo.id === id);
-      if (todo) {
-        todo.text = text;
-      }
-    },
-    deleteTodos: (state) => {
-      state.todos = [];
-    },
   },
 });
 export const { addTodo, removeTodo, updateTodo, deleetTodos } =
