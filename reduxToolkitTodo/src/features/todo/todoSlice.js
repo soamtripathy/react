63398134f544814cf068ca8ae60422aa2b46a1ed
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-05-05 16:58:43 +0530

    reducer

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -8,7 +8,29 @@ export const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
-    addTodo: (state, action) => {},
-    removeTodo: () => {},
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.text = text;
+      }
+    },
+    deleteTodos: (state) => {
+      state.todos = [];
+    },
   },
 });
+export const { addTodo, removeTodo, updateTodo, deleetTodos } =
+  todoSlice.actions;
+
+export default todoSlice.reducer;
