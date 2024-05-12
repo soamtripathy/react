13929c4241e818ae2b03144f413c9b7ecad76b05
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-05-12 19:47:15 +0530

    form component

---
+++ 12megaBlog/src/components/Select.jsx
@@ -0,0 +1,26 @@
+import React, { useId } from "react";
+
+function Select({ options, label, className = "", ...rest }) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && <label htmlFor={id} className=""></label>}
+      <select
+        ref={ref}
+        id={id}
+        {...rest}
+        className={`px-3 py-2
+      rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200
+      border border-gray-200 w-full ${className}`}
+      >
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default React.forwardRef(Select);
